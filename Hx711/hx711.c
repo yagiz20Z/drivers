cef8ec5664feda065700ab4c96a141cc3e9532de
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-09-25 16:17:21 +0300

    Hx711 kütüphanesi eklenecek

---
+++ Hx711/hx711.c
@@ -0,0 +1,8 @@
+/*
+ * hx711.c
+ *
+ *  Created on: Sep 25, 2025
+ *      Author: aliya
+ */
+
+
---
+++ Hx711/hx711.h
@@ -0,0 +1,13 @@
+/*
+ * hx711.h
+ *
+ *  Created on: Sep 25, 2025
+ *      Author: aliya
+ */
+
+#ifndef INC_HX711_H_
+#define INC_HX711_H_
+
+
+
+#endif /* INC_HX711_H_ */
